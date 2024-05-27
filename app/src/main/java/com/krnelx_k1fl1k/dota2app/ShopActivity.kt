72f354afd1afc2e631ff4153eedd53aa7a7c3029
kovmak <kovmak15@gmail.com>
2024-05-27 23:51:17 +0300

    Update visual and adding logo app

--- app/src/main/java/com/krnelx_k1fl1k/dota2app/ShopActivity.kt
+++ app/src/main/java/com/krnelx_k1fl1k/dota2app/ShopActivity.kt
@@ -1,14 +1,26 @@
 package com.krnelx_k1fl1k.dota2app
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class ShopActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_shop)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     fun WhatsNewClick(view: View) {
--- app/src/main/java/com/krnelx_k1fl1k/dota2app/WhatsNewActivity.kt
+++ app/src/main/java/com/krnelx_k1fl1k/dota2app/WhatsNewActivity.kt
@@ -1,14 +1,25 @@
 package com.krnelx_k1fl1k.dota2app
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class WhatsNewActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_new)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     fun ShopClick(view: android.view.View){
